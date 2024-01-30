8cede3032b713312ce98447e47e0f470e0649b81
User master <toworkallmylife@gmail.com>
2024-01-30 22:12:19 +0300

    add num1 two

---
+++ num1.cs
@@ -0,0 +1,12 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    int a = 5;
+    int b = 11;asd
+
+
+    asd
+    asd
+    Console.WriteLine($"Hello World it is my second debug!!! {a + b + 5}");
+  }
+}

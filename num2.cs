9429ab207b5b2fdf79ba631cbeee5da83646baa8
User master <toworkallmylife@gmail.com>
2024-01-30 22:17:54 +0300

    add

---
+++ num2.cs
@@ -0,0 +1,8 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World it is my first debug!!!");
+    int a = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(a + 1231);
+  }
+}

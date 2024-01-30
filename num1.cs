f40e160997cf0d8e16300e8185b25512e6e62daa
User master <toworkallmylife@gmail.com>
2024-01-30 22:25:35 +0300

    del nums

--- num1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class HelloWorld {
-  static void Main() {
-    int a = 5;
-    int b = 11;
-    Console.WriteLine($"Hello World it is my second debug!!! {a + b + 5}");
-  }
-}
--- num2.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class HelloWorld {
-  static void Main() {
-    Console.WriteLine("Hello World it is my first debug!!!");
-    int a = Convert.ToInt32(Console.ReadLine());
-    System.Console.WriteLine(a + 1231);
-  }
-}
